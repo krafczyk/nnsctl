b938301e081b149592c22032569de5689cc0071a
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-10-04 10:38:28 -0500

    First version with a working vpn, not quite what I want though.

--- vpn_manager.sh
+++ vpn_manager.sh
@@ -64,20 +64,36 @@ setup_vpn() {
     sudo iptables -A FORWARD -o $WIRED_IF -i $OUT_IF -j ACCEPT
     sudo iptables -A FORWARD -i $WIRED_IF -o $OUT_IF -j ACCEPT
 
-    $NS_EXEC ping -c 5 127.0.0.1
-    $NS_EXEC ping -c 5 localhost
-    $NS_EXEC ping -c 5 8.8.8.8
-    $NS_EXEC ping -c 5 192.168.2.1
-    $NS_EXEC ping -c 5 google.com
-    $NS_EXEC ping -c 5 ${VPN_ENDPOINT}
+    $NS_EXEC ping -c 3 127.0.0.1
+    $NS_EXEC ping -c 3 localhost
+    $NS_EXEC ping -c 3 8.8.8.8
+    $NS_EXEC ping -c 3 192.168.2.1
+    $NS_EXEC ping -c 3 ${VPN_ENDPOINT}
+    $NS_EXEC ping -c 3 google.com
 
-    ## Run OpenConnect in the namespace
-    #echo "Starting VPN..."
-    #$NS_EXEC openconnect -b --interface $vpn_interface --pid-file=$vpn_pid_file --user=${VPN_USER} --pro#tocol=anyconnect $VPN_ENDPOINT
-    ##$NS_EXEC openconnect -b --interface $vpn_interface --user=${VPN_USER} --protocol=anyconnect $VPN_ENDPOINT
+    # Run OpenConnect in the namespace
+    echo "Starting VPN..."
+    ##$NS_EXEC openconnect -b --interface $vpn_interface --pid-file=$vpn_pid_file --user=${VPN_USER} --protocol=anyconnect $VPN_ENDPOINT
+    $NS_EXEC openconnect -b --interface $vpn_interface --user=${VPN_USER} --protocol=anyconnect $VPN_ENDPOINT
 
-    ## Wait for $vpn_inteface interface to be created
-    #while ! $NS_EXEC ip link show dev $vpn_interface >/dev/null 2>&1; do sleep .5; done;
+    # Wait for $vpn_inteface interface to be created
+    while ! $NS_EXEC ip link show dev $vpn_interface >/dev/null 2>&1; do sleep .5; done;
+
+    echo "post vpn tests"
+
+    $NS_EXEC ping -c 3 127.0.0.1
+    $NS_EXEC ping -c 3 localhost
+    $NS_EXEC ping -c 3 8.8.8.8
+    $NS_EXEC ping -c 3 192.168.2.1
+    $NS_EXEC ping -c 3 ${VPN_ENDPOINT}
+    $NS_EXEC ping -c 3 google.com
+
+    echo "ip route show"
+    ip route show
+    echo "ip addr show"
+    ip addr show
+    echo "sudo ip netns pids"
+    sudo ip netns pids $NS_NAME
 
     ## Enable IP forwarding and NAT
     #echo "Creating iptables rules"
@@ -89,10 +105,21 @@ teardown_vpn() {
     ## If VPN_PID is not 0, kill it
     ##VPN_PID=$(cat $vpn_pid_file)
     ##sudo kill -SIGINT $VPN_PID || true
-    #echo "Stopping VPN and other processes within the network namespace $NS_NAME"
-    #sudo ip netns pids $NS_NAME | sudo xargs -rd'\n' kill -SIGINT
-#
-#    sleep 2
+    echo "Stopping VPN and other processes within the network namespace $NS_NAME"
+    pids=$(sudo ip netns pids $NS_NAME)
+    # First try to terminate
+    for pid in $pids; do
+        sudo kill -TERM $pid
+    done
+
+    # Wait for processes to terminate
+    sleep 5
+
+    for pid in $pids; do
+        if sudo kill -0 $pid 2>/dev/null; then
+            sudo kill -KILL $pid
+        fi
+    done 
 
     ## Remove port forwarding and NAT
     #echo "Removing iptables rules"
@@ -124,8 +151,5 @@ trap teardown_vpn EXIT SIGINT SIGTERM
 
 setup_vpn
 
-## Wait for the VPN process to finish
-#VPN_PID=$(cat $vpn_pid_file)
-#if [ $VPN_PID -ne 0 ]; then
-#    wait $VPN_PID
-#fi
+# Wait for processes in the network namespace to finish
+while sudo ip netns pids $NS_NAME | grep -q .; do sleep .5; done;
