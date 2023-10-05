471d891d0d32b578641265e66563bc07a54d0bdb
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-10-05 16:59:44 -0500

    Update option parsing for launch script

--- launch_in_vpn.sh
+++ launch_in_vpn.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+# Find and load default values from config file
+CONFIG_FILE="$(dirname "$0")/vpn_config.sh"
+if [ -f "$CONFIG_FILE" ]; then
+  source "$CONFIG_FILE"
+else
+  echo "Config file vpn_config.sh not found. Using built-in defaults."
+fi
+
+# Usage function for help text
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -n, --ns-name       Namespace Name"
+  echo "  --help              Display this help text and exit"
+}
+
+# Parse command line arguments
+TEMP=$(getopt -o n:: --long ns-name::help -n "$0" -- "$@")
+
+eval set -- "$TEMP"
+
+while true; do
+    case "$1" in
+        -n|--ns-name) NS_NAME="$2"; shift 2;;
+        --help) usage; exit 0;;
+        --) shift; break;;
+        *) echo "Invalid option"; usage; exit 1;;
+    esac
+done
+
 # Default namespace
-NS_NAME="myvpn"
+NS_NAME="nn_vpn"
 
 # Check for namespace argument
 if [ "$1" == "--namespace" ]; then
---
+++ vpn_config.sh
@@ -0,0 +1,7 @@
+# Default values
+VPN_IF="vpn0"
+VPN_ENDPOINT="vpn.illinois.edu"
+USER="mkrafcz2"
+AUTHGROUP="3_TunnelAll"
+NS_NAME="nn_vpn"
+SUBNET=192.168.3
